88ec705227a2fa743b55b0d844df79d832577cf8
Roman Navarro <ronnoverro@gmail.com>
2024-07-17 13:45:01 -0700

    collision explosion added

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] ParticleSystem explodeParticles;
+
+    [Tooltip("Delay before restarting level")] 
+    [SerializeField] float delay = 2f;
+
     void Update()
     {
         Debug.Log("fucking work");
@@ -16,13 +23,31 @@ public class CollisionHandler : MonoBehaviour
         var playerName = gameObject.tag;
         Debug.Log(playerName + " collided with " + other.gameObject.tag);
         // Debug.Log("fucking collision!!!");
+
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", delay);
     }    
 
     // to work, both objs need to have "Is Trigger" disabled
     void OnCollisionEnter(Collision other) {
         Debug.Log("Actual FUCKING COLLISION");
 
+        explodeParticles.Play();
+
         // USING STRING INTERPOLATION:
         Debug.Log($"{this.name} collided with {other.gameObject.name}");
+
+        // script disables itself lmfao
+        GetComponent<PlayerControls>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+
+        Invoke("ReloadLevel", delay);
+        // Destroy(gameObject);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;   // get the index of the active scene.
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
